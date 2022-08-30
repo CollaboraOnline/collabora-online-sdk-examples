f64eff43525e05256daec7aef2fe47e21d841b3a
Abi Lowrie <abigail.lowrie@foobarengineering.co.uk>
2022-08-30 08:47:46 +0100

    Coded changes made mid-commentation

--- webapp/dotNET/WebApplication1/Controllers/FileControllers.cs
+++ webapp/dotNET/WebApplication1/Controllers/FileControllers.cs
@@ -7,45 +7,61 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class FileControllers : Controller
     {
+        // .... NEED TO FIGURE OUT WHAT DOES
         private readonly ILogger<FileControllers> _logger;
 
         public FileControllers(ILogger<FileControllers> logger)
         {
             _logger = logger;
         }
+
         [HttpGet("/wopi/files/{fileId}")]
-        public IEnumerable<FileInfoTemplate> CheckFileInfo()
+        // This endpoint gets the information of the file, which id value is being used in the endpoint and returns it 
+        //  to the host machiene that is running the webpage from the collabora server. 
+        public ActionResult<FileInfoTemplate> CheckFileInfo()
         {
-            //_logger.LogInformation("file id: " + /*req.params.fileId*/);
-            return Enumerable.Range(0, 1).Select(index => new FileInfoTemplate
+            //_logger.LogInformation("file id: " + /*req.params.fileId*/); *This is still in progress but is meant to write the information to the logger.*
+            return new FileInfoTemplate
             {
                 BaseFileName = "test.txt",
                 Size = 11,
                 UserId = 1,
                 UserCanWrite = true
-            })
-            .ToArray();
+            };
         }
+        
         [HttpGet("/wopi/files/{fileId}/contents")]
+        // This endpoint gets the contents of the file, as this a SDK example the content loaded onto the file origionally
+        //  is hardcoded into the system. This is what makes sure when a new page of the SDK is opened it always opens with 
+        //  the text 'Hello World'. 
         public IActionResult GetFile()
         {
             var fileContent = "Hello World";
             return base.Content(fileContent);
         }
+
+  //// Post endpoint to be made to work 
+
         //[HttpPost("/wopi/files/{fileId}/contents")]
         //public IActionResult PutFile()
         //{
-        //    _logger.LogInformation("wopi PutFile endpoint");
-        //    if (HttpRequest.Body)
+        //    using (StreamReader stream = new StreamReader(HttpContext.Request.Body))
         //    {
-        //        System.Diagnostics.Debug.WriteLine(HttpRequest.Body);
-        //        _logger.LogInformation(HttpRequest.Body.tostring());
-        //        return StatusCode(200);
-        //    }
-        //    else
-        //    {
-        //        _logger.LogInformation("Not possible to get the file content.");
-        //        return StatusCode(404);
+        //        string body = stream.ReadToEnd();
+        //        // body = "param=somevalue&param2=someothervalue"
+
+        //        _logger.LogInformation("wopi PutFile endpoint");
+        //        if (body!=null)
+        //        {
+        //            System.Diagnostics.Debug.WriteLine(body);
+        //            _logger.LogInformation(body);
+        //            return StatusCode(200);
+        //        }
+        //        else
+        //        {
+        //            _logger.LogInformation("Not possible to get the file content.");
+        //            return StatusCode(404);
+        //        }
         //    }
         //}
     }
--- webapp/dotNET/WebApplication1/Program.cs
+++ webapp/dotNET/WebApplication1/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(opition=>opition.JsonSerializerOptions.PropertyNamingPolicy=null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,14 +12,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    //app.Urls = "Index.html";
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
+
 app.Run();
+
