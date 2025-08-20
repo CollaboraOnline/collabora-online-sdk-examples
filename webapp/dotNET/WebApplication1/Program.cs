d861a0331e5eab76d897928e85ea7bf556ebab97
Moritz Duge <moritz.duge@collabora.com>
2025-08-20 14:31:11 +0200

    dotNET example: revamp most of the example
    - update from dotNET version 6.0 to 9.0
    - use common HTML templates
    - ask user for Collabora Online URL instead of hardcoding it
    - load "onlineUrl" from discovery.xml instead of hardcoding it
    - added README.md
    - add HTTPS/TLS/SSL mode
    - set logging level to include HTTP requests
    - respect environment variables: PORT, SSL_KEY_FILE, SSL_CRT_FILE,
      DISABLE_TLS_CERT_VALIDATION
    - much more ...

--- webapp/dotNET/WebApplication1/Program.cs
+++ webapp/dotNET/WebApplication1/Program.cs
@@ -1,25 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Security.Cryptography.X509Certificates;
+
+
+// Uses the Kestrel HTTP server trough the ASP.NET Core middleware.
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions {
+  Args = args,
+  WebRootPath = "../../html"
+});
+
+ushort port;
+if (!ushort.TryParse(Environment.GetEnvironmentVariable("PORT"), out port)) port = 3000;
+
+var tls_crt = Environment.GetEnvironmentVariable("SSL_CRT_FILE");
+var tls_key = Environment.GetEnvironmentVariable("SSL_KEY_FILE");
+if (String.IsNullOrEmpty(tls_crt) || String.IsNullOrEmpty(tls_key)) {
+  builder.WebHost.ConfigureKestrel(options => {
+    options.ListenAnyIP(port);
+    if (port != 5123) { options.ListenAnyIP(5123); }
+    if (port != 7123) {
+      options.ListenAnyIP(7123, listenOptions => { listenOptions.UseHttps(); });
+    }
+  });
+} else {
+  builder.WebHost.ConfigureKestrel(options => {
+    options.ListenAnyIP(port, listenOptions => {
+      listenOptions.UseHttps(X509Certificate2.CreateFromPemFile(tls_crt!, tls_key!));
+    });
+  });
+}
 
 // Add services to the container.
-
-builder.Services.AddControllers().AddJsonOptions(opition=>opition.JsonSerializerOptions.PropertyNamingPolicy=null);
+builder.Services.AddControllers().AddJsonOptions(
+  opition=>opition.JsonSerializerOptions.PropertyNamingPolicy=null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();  // HTTP client request for discovery.xml
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()) {
+  // hard enabled in: Properties/launchSettings.json
+  // for debugging browse to: http(s)://HOSTNAME/swagger/
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
-
-app.UseAuthorization();
-
+app.UseDefaultFiles();  // default to index.html
+app.UseStaticFiles();   // serve web root
 app.MapControllers();
 
-
 app.Run();
-
